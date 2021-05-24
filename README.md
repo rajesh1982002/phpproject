591afb316ce4c84a87c90e83248e920d537d99f5
rajesh1982002 <rajesh1982002@gituser.com>
2021-05-24 16:04:55 -0500

    where is my commit

--- README.md
+++
@@ -1,5 +0,0 @@
-# phpproject
-a new php project
-
-a brand new php project 
-lets see what it has to offer.
--- index.php
+++ index.php
@@ -4,3 +4,9 @@ echo "Today is " . date("Y.m.d") . "<br>";
 echo "Today is " . date("Y-m-d") . "<br>";
 echo "Today is " . date("l");
 ?>
+<?php
+$name = "Quincy";
+echo "Hi! My name is " . $name . "<br>";
+echo "Hi! My name is " . $NAME . "<br>";
+echo "Hi! My name is " . $NaMe . "<br>";
+?>
